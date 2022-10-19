17549ec4564653bfc74a52d813e25b3258b29dcd
wtfzambo <zambelli.federico@gmail.com>
2022-10-19 09:10:56 +0000

    fix: update broken model reference

--- greenery/models/marts/marketing/user_order_facts.sql
+++ greenery/models/marts/marketing/user_order_facts.sql
@@ -9,13 +9,17 @@ users AS (
     SELECT * FROM {{ ref('int_users_addresses_events') }}
 )
 
+, orders AS (
+    SELECT * FROM {{ ref('fct_orders') }}
+)
+
 , orders_stats_per_user AS (
     SELECT user_guid
      , date_trunc('day', min(created_at_utc)) AS first_order_date
      , date_trunc('day', max(created_at_utc)) AS last_order_date
      , count(distinct order_guid) AS total_user_orders
      , sum(order_total) AS user_lifetime_value
-  FROM fct_orders
+  FROM orders
  GROUP BY 1
 )
 
